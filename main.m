14083124d6df5d2720c041384c86c9a69b9fbd76
Matthew Copeland <bamamat1724@aim.com>
2021-03-28 18:53:50 -0400

    bug duplicate samprate declaration and added noise

--- main.m
+++ main.m
@@ -2,35 +2,45 @@ clc;
 close all;
 clear all;
 
+%constants
 c = 299.8E6;%speed of light
 boltz = 1.3806485E-23;%boltzman const
 
-sampleRate=10E6;%Hz
-PRF = 2000;
+%simple Radar parameters
+PRF=2000;
+nPulse=16;
 PRI=1/PRF;%seconds
 fc=1E9;%Hz
 txpower=40;%dBm
 txgain=0;%dBi
 rxgain=0;%dBi
+receiverTemp=300;
+receiverBW=1000000;
 
+%simple signal parameters
 sampPerSym = 30; %samples per symbol
 symRate = 250000; %symbols per second
 Nsym = 30; %number of symbols in a message
 Nc = 8; %number of orthogonal carriers
 
+%basic signal calcs
 signal = buildWaveform(sampPerSym,symRate,Nc,Nsym,0);
+sampRate = signal.sampRate;
 bandWidth = (Nc+1)*Nsym;
-pulseWidth=length(signal.IQ)/signal.sampRate;
+pulseWidth=length(signal.IQ)/sampRate;
 
-minRange=pulseWidth*c/2;
-maxRange=c*(PRI-pulseWidth)/2;
+%basic radar calcs
+minUnambigRange=pulseWidth*c/2;
+maxUnambigRange=c*(PRI-pulseWidth)/2;
 
-receiverTemp=300;
-receiverBW=1000;
+minRange = ceil(minUnambigRange/1000)*1000;
+maxRange = floor(maxUnambigRange/1000)*1000;
 
+%comms postitions
 txAntPos = [20000,0,10];
 rxAntPos = [0,0,0];
 
+%radar targets
 targets(1).rcs = 0;
 targets(1).initialPos = [0,0,0];
 targets(1).velocity = [0,0,0];
@@ -41,4 +51,10 @@ targets(3).rcs = 10;
 targets(3).initialPos = [-1000,0,0];
 targets(3).velocity = [0,0,0];
 
-plotPSD(3000,sampPerSym,symRate,Nc,Nsym)
+dataCubeLength = floor(((maxRange - minRange)/c + pulseWidth)*sampRate);
+
+noisePow = boltz*receiverBW*receiverTemp;
+dataCube = noisePow*(randn(nPulse,dataCubeLength)+1i*randn(nPulse,dataCubeLength));
+
+%math stuff
+%plotPSD(3000,sampPerSym,symRate,Nc,Nsym)
